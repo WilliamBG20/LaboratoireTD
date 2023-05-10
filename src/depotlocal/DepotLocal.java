4a6ae0e9fd0b32382976eefc77b1344b786d57e9
lilwi <lilwi@172.29.207.164>
2023-05-10 15:34:51 -0400

    Commit Method Hello World

--- src/depotlocal/DepotLocal.java
+++ src/depotlocal/DepotLocal.java
@@ -14,7 +14,7 @@ public class DepotLocal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Hello World!");
     }
     
 }
